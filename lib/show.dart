20fce6787cb0263c61e401a68b9ce6f366e2edf6
Eduardo M <contact@eduardom.dev>
2023-06-18 14:29:40 +0200

    New show mode banner

--- lib/show.dart
+++ lib/show.dart
@@ -1,5 +1,6 @@
 import 'package:chromapulse/providers/artnet.dart';
 import 'package:chromapulse/providers/providers.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -17,20 +18,30 @@ class _ShowPageState extends ConsumerState<ShowPage> {
   // Show material banner on page load
   @override
   void didChangeDependencies() {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      _showMaterialBanner();
-    });
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
     Wakelock.enable();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
     super.didChangeDependencies();
   }
 
   @override
   void dispose() {
+    cleanUp();
+    super.dispose();
+  }
+
+  void cleanUp() {
     ScreenBrightness().resetScreenBrightness();
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
     Wakelock.disable();
-    super.dispose();
   }
 
   late ArtnetState? artnetState;
@@ -54,59 +65,128 @@ class _ShowPageState extends ConsumerState<ShowPage> {
     return Scaffold(
       body: GestureDetector(
         onDoubleTap: () {
-          if (_isBannerVisible) {
-            return;
-          }
-          // Show material banner on double tap
-          _showMaterialBanner();
+          setState(() {
+            _isBannerVisible = true;
+          });
         },
         behavior: HitTestBehavior.opaque,
-        child: Center(
-          child: Container(
-            color: Color.fromARGB(
-              255,
-              artnetState?.red.value ?? 0,
-              artnetState?.green.value ?? 0,
-              artnetState?.blue.value ?? 0,
+        child: Stack(
+          children: [
+            Positioned(
+              child: Container(
+                width: double.infinity,
+                height: double.infinity,
+                color: Color.fromARGB(
+                  255,
+                  artnetState?.red.value ?? 0,
+                  artnetState?.green.value ?? 0,
+                  artnetState?.blue.value ?? 0,
+                ),
+              ),
             ),
-          ),
+            AnimatedPositioned(
+              duration: const Duration(milliseconds: 300),
+              bottom: _isBannerVisible ? 20 : -200,
+              right: 0,
+              left: 0,
+              child: Card(
+                  shadowColor: Colors.white.withOpacity(0.2),
+                  elevation: 10,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "Show Mode",
+                              style: Theme.of(context).textTheme.headlineSmall,
+                            ),
+                            const StatusBadge(),
+                          ],
+                        ),
+                        const Divider(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text("Last Packet: "),
+                                Row(
+                                  children: [
+                                    Text(
+                                      "R: ${artnetState?.red.value ?? 0}",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodySmall!
+                                          .copyWith(
+                                            color: Colors.red,
+                                          ),
+                                    ),
+                                    Text(
+                                      " G: ${artnetState?.green.value ?? 0}",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodySmall!
+                                          .copyWith(
+                                            color: Colors.green,
+                                          ),
+                                    ),
+                                    Text(
+                                      " B: ${artnetState?.blue.value ?? 0}",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodySmall!
+                                          .copyWith(
+                                            color: Colors.blue,
+                                          ),
+                                    ),
+                                    if (is4Channel)
+                                      Text(
+                                        " Br: ${artnetState?.brightness.value ?? 0}",
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodySmall!
+                                            .copyWith(
+                                              color: Colors.white,
+                                            ),
+                                      ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                OutlinedButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      _isBannerVisible = false;
+                                    });
+                                  },
+                                  child: const Text("Hide"),
+                                ),
+                                IconButton(
+                                  onPressed: () {
+                                    cleanUp();
+                                    Navigator.of(context).pop();
+                                  },
+                                  icon: const Icon(CupertinoIcons.settings),
+                                ),
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  )),
+            ),
+          ],
         ),
       ),
     );
-  }
-
-  void _showMaterialBanner() {
-    _isBannerVisible = true;
-    ScaffoldMessenger.of(context).showMaterialBanner(MaterialBanner(
-      content: Row(
-        children: [
-          const StatusBadge(),
-          const SizedBox(width: 8),
-          IconButton(
-            onPressed: () {
-              _isBannerVisible = false;
-              // Exit show page
-              ScaffoldMessenger.of(context).clearMaterialBanners();
-              // Delay exit to allow material banner to be removed
-              Future.delayed(const Duration(milliseconds: 500), () {
-                if (mounted) Navigator.of(context).pop();
-                ref.read(artnetStateProvider.notifier).dispose();
-              });
-            },
-            icon: const Icon(Icons.settings),
-          ),
-        ],
-      ),
-      actions: [
-        OutlinedButton(
-          onPressed: () {
-            ScaffoldMessenger.of(context).hideCurrentMaterialBanner();
-            _isBannerVisible = false;
-          },
-          child: const Text("Hide"),
-        ),
-      ],
-    ));
   }
 }
 
